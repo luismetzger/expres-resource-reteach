14ccc42c6ef3c805173a657bdc90ddb9c528656e
Luis Metzger <luismetzger@me.com>
2016-07-07 08:13:22 -0600

    Most of this homework complete. Still have questions about PUT and Del.

--- starter_code/server.js
+++ starter_code/server.js
@@ -1,6 +1,9 @@
 'use strict'
-var express = require('express')
-var app = express() 
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 var candies = [
   {
@@ -19,6 +22,48 @@ var candies = [
     "color": "Pink"
   }
 ];
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/candies', function(req, res) {
+    res.send(candies);
+});
+
+app.get('/candies/:id', function(req, res) {
+    var candies_id = candies[req.params.id];
+    if(candies.length <= req.params.id || req.params.id < 0 || req.params.id === undefined) {
+        res.statusCode = 404;
+        return res.send('Error 404: No candy found');
+    }
+    res.send(candies_id);
+});
+
+app.post('/candies', function(req, res) {
+    var newCandy = {
+        "id" : req.body.id,
+        "name" : req.body.name,
+        "color" : req.body.color
+    }
+    candies.push(newCandy);
+    res.json(true);
+});
+
+app.post('/candies', function(req, res) {
+    var newCandy = {
+        "name" : req.body.name,
+        "color" : req.body.color
+    }
+    candies.push(newCandy);
+    res.json(true);
+});
+
+app.put('/candies/:id', function(req, res) {
+    
+
+});
+
 
 app.listen(3000)
 console.log("Server listening on port 3000")
